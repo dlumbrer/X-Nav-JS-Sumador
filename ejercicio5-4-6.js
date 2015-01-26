29863f6656edf9edc713b418247c8ea4d7c5030e
david moreno lumbreras <dlumbrer@lambda26.aulas.gsyc.es>
2015-01-26 14:49:01 +0100

    Finalizado ejercicio de entrega 5-4-6

--- ejercicio5-4-6.js
+++ ejercicio5-4-6.js
@@ -2,8 +2,16 @@ function changer (id, newValue) {
     var element = document.getElementById(id);
     element.innerHTML = newValue;
 }
+
 function sumar(tagsuma, tagresultado){  
     elementos_suma = document.getElementById(tagsuma);
     elementos = elementos_suma.innerHTML.split("+", 2);
-    changer(tagresultado, (parseInt(elementos[0])+parseInt(elementos[1])));
+    changer(tagresultado, ("= " + (parseInt(elementos[0])+parseInt(elementos[1]))));
+}
+
+
+function otrasuma(tagsuma, tagresultado){
+    elementos_suma = document.getElementById(tagsuma);
+    elementos_suma.innerHTML = parseInt((Math.random()*1000000)) + "+" + parseInt((Math.random()*1000000))
+    sumar(tagsuma,tagresultado);
 }
